0a8d59e98dc157e4c5373ca37ccf2d99632b5108
Manu Febie <manu.febie@gmail.com>
2017-10-30 19:28:50 +0700

    Deleted ideas app and created news app.

--- news/models.py
+++ news/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 
-class Idea(models.Model):
-    name = models.CharField(max_length=255)
+class News(models.Model):
+    title = models.CharField(max_length=255)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.title
